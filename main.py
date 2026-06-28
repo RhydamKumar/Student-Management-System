6e3c151f2901b944dc1faf1a876f4d39269c0d27
RhydamKumar <rhydamkumar2004@gmail.com>
2026-06-28 22:26:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,22 +4,28 @@ def menu():
     while True:
         print("\n ========================= Student Management System =========================")
         print("1. Add Student")
-        print("2. Update Marks")
-        print("3. Show All Student")
-        print("4. Exit")
+        print("2. Display All Students")
+        print("3. Search Student by ID/Name")
+        print("4. Update Student Information")
+        print("5. Delete Student")
+        print("6. Exit")
 
-        choice = (input("Enter your Option (1-4): "))
+        choice = input("Enter your Option (1-6): ")
         if choice == '1':
             Student.add_student()
         elif choice == '2':
-            Student.update_student_marks()
-        elif choice == '3':
             Student.show_all_students()
+        elif choice == '3':
+            Student.search_student()
         elif choice == '4':
+            Student.update_student()
+        elif choice == '5':
+            Student.delete_student()
+        elif choice == '6':
             print("Exiting Student Management System. Goodbye!")
             break
         else:
-            print ("Invalid choice. Please Enter between 1-4")
+            print("Invalid choice. Please Enter between 1-6")
 
-if __name__=="__main__":
-     menu()
+if __name__ == "__main__":
+    menu()
